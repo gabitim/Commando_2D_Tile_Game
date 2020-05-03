dd8e6862455e2c8cb701975fd156217d48c0f5ea
Timofti Gabriel <gabitim470@gmail.com>
2020-05-03 19:50:22 +0300

    Game Loop and reducing frames

--- src/com/commando/game/GamePanel.java
+++ src/com/commando/game/GamePanel.java
@@ -46,27 +46,74 @@ public class GamePanel extends JPanel implements Runnable{
 
     private int x = 0;
 
+    public void input() {
+
+    }
+
     public void update() {
         x ++;
         System.out.println(x);
     }
 
     public void render() {
-
+        if( g!= null) {
+            g.setColor(new Color(107, 213, 244));
+            g.fillRect(0,0, width, height);
+        }
     }
 
     public void draw() {
-
+        Graphics g2 = (Graphics)this.getGraphics();
+        g2.drawImage(img, 0, 0, width, height, null);
+        g2.dispose();
     }
+
 
     @Override
     public void run() {
         init();
 
+        final double GAME_FPS = 60.0 ;
+        final double TIME_BEFORE_UPDATE = 1_000_000_000 / GAME_FPS;
+        final int UPDATES_BEFORE_RENDER = 5;
+
+        double lastUpdateTime = System.nanoTime();
+        double lastRenderTime;
+
+        final double TARGET_FPS = 60;
+        final double TOTAL_TIME_BEFORE_RENDER = 1_000_000_000 / TARGET_FPS;
+
         while (running) {
-            update();
+            double now = System.nanoTime();
+            int updateCount = 0;
+            while (( (now - lastUpdateTime) > TIME_BEFORE_UPDATE) && (updateCount < UPDATES_BEFORE_RENDER) ){
+                update();
+                input();
+                lastUpdateTime += TIME_BEFORE_UPDATE;
+                updateCount++;
+            }
+
+            if(now - lastUpdateTime > TIME_BEFORE_UPDATE) {
+                lastUpdateTime = now - TIME_BEFORE_UPDATE;
+            }
+
+            input();
             render();
             draw();
+
+            lastRenderTime = now;
+
+            while (now - lastRenderTime < TOTAL_TIME_BEFORE_RENDER && now - lastUpdateTime < TIME_BEFORE_UPDATE) {
+                Thread.yield();
+
+                try {
+                    Thread.sleep(1);
+                } catch (Exception e) {
+                    System.out.println("ERROR: yielding thread");
+                }
+                now = System.nanoTime();
+            }
+
         }
     }
 }

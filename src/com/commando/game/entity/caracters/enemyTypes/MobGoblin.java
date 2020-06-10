a0bbb32327771afcf9482c0b3284133cb3668272
Timofti Gabriel <gabitim470@gmail.com>
2020-06-10 21:36:46 +0300

    Last tests. Documentation added

--- src/com/commando/game/entity/caracters/enemyTypes/MobGoblin.java
+++ src/com/commando/game/entity/caracters/enemyTypes/MobGoblin.java
@@ -1,12 +1,13 @@
 package com.commando.game.entity.caracters.enemyTypes;
 
+import com.commando.game.entity.caracters.Enemy;
 import com.commando.game.graphics.SpriteSheet;
 import com.commando.game.util.Vector2d;
 
 /**
  * @author Timofti Gabriel
  */
-public class MobGoblin extends MobSkeleton {
+public class MobGoblin extends Enemy {
     public MobGoblin(SpriteSheet spriteSheet, Vector2d origin, int size) {
         super(spriteSheet, origin, size);
     }
